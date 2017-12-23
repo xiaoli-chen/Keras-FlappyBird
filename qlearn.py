157ff93909146b7c1af50a4e2e99c7510f9b6cd6
Xiaoli Chen <crazychen@Xiaolis-MacBook-Pro.local>
2017-12-23 11:36:32 +0800

    comments

--- qlearn.py
+++ qlearn.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+# 申明调包
 from __future__ import print_function
 
 import argparse
@@ -14,7 +16,7 @@ import numpy as np
 from collections import deque
 
 import json
-from keras import initializers
+from keras import initializers  # keras 包调用，最新版的名称有变动
 from keras.initializers import normal, identity
 from keras.models import model_from_json
 from keras.models import Sequential
@@ -23,50 +25,51 @@ from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD , Adam
 import tensorflow as tf
 
-GAME = 'bird' # the name of the game being played for log files
+GAME = 'bird' # 日志文件中游戏的名称
 CONFIG = 'nothreshold'
-ACTIONS = 2 # number of valid actions
-GAMMA = 0.99 # decay rate of past observations
-OBSERVATION = 3200. # timesteps to observe before training
+ACTIONS = 2 # 有效动作的数量
+GAMMA = 0.99 # 衰减速度
+OBSERVATION = 3200. # 开始训练前的观测时长 timesteps to observe before training
 EXPLORE = 3000000. # frames over which to anneal epsilon
-FINAL_EPSILON = 0.0001 # final value of epsilon
-INITIAL_EPSILON = 0.1 # starting value of epsilon
-REPLAY_MEMORY = 50000 # number of previous transitions to remember
-BATCH = 32 # size of minibatch
+FINAL_EPSILON = 0.0001 # epsilon的最终值
+INITIAL_EPSILON = 0.1 # epsilon的其实值
+REPLAY_MEMORY = 50000 # 过往的状态转移数量
+BATCH = 32 # minibatch批处理的大小
 FRAME_PER_ACTION = 1
 LEARNING_RATE = 1e-4
 
 img_rows , img_cols = 80, 80
 #Convert image into Black and white
-img_channels = 4 #We stack 4 frames
+img_channels = 4 # 4幅画面叠在一起进行学习
 
 def buildmodel():
+    # 以下利用卷积神经网进行图像识别
     print("Now we build the model")
     model = Sequential()
     model.add(Convolution2D(32, 8, 8, subsample=(4, 4), border_mode='same',input_shape=(img_rows,img_cols,img_channels)))  #80*80*4
-    model.add(Activation('relu'))
+    model.add(Activation('relu')) # ReLU 激活函数
     model.add(Convolution2D(64, 4, 4, subsample=(2, 2), border_mode='same'))
-    model.add(Activation('relu'))
+    model.add(Activation('relu')) # ReLU 激活函数
     model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='same'))
-    model.add(Activation('relu'))
+    model.add(Activation('relu')) # ReLU激活函数
     model.add(Flatten())
-    model.add(Dense(512))
-    model.add(Activation('relu'))
+    model.add(Dense(512))     # fully connected 隐式层
+    model.add(Activation('relu')) # ReLU激活函数
     model.add(Dense(2))
    
-    adam = Adam(lr=LEARNING_RATE)
-    model.compile(loss='mse',optimizer=adam)
+    adam = Adam(lr=LEARNING_RATE) # 优化方法， Adam
+    model.compile(loss='mse',optimizer=adam) # Loss function 定义，MSE，使用ADAM进行优化
     print("We finish building the model")
     return model
 
 def trainNetwork(model,args):
-    # open up a game state to communicate with emulator
-    game_state = game.GameState()
+    # 从游戏模拟器中识别游戏状态
+    game_state = game.GameState() #flappy bird小游戏的状态
 
-    # store the previous observations in replay memory
+    # 将过去的观察记录保存
     D = deque()
 
-    # get the first state by doing nothing and preprocess the image to 80x80x4
+    #初始化首幅图片，将大小调整为 80x80x4
     do_nothing = np.zeros(ACTIONS)
     do_nothing[0] = 1
     x_t, r_0, terminal = game_state.frame_step(do_nothing)
@@ -78,20 +81,20 @@ def trainNetwork(model,args):
     s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
     #print (s_t.shape)
 
-    #In Keras, need to reshape
+    # 在Keras中，需要注意的是输入张量的形状和纬度大小 
     s_t = s_t.reshape(1, s_t.shape[0], s_t.shape[1], s_t.shape[2])  #1*80*80*4
 
     
 
     if args['mode'] == 'Run':
-        OBSERVE = 999999999    #We keep observe, never train
+        OBSERVE = 999999999    # 观测，跑，不训练模型
         epsilon = FINAL_EPSILON
         print ("Now we load weight")
         model.load_weights("model.h5")
         adam = Adam(lr=LEARNING_RATE)
         model.compile(loss='mse',optimizer=adam)
         print ("Weight load successfully")    
-    else:                       #We go to training mode
+    else:                       # 训练模型
         OBSERVE = OBSERVATION
         epsilon = INITIAL_EPSILON
 
@@ -102,19 +105,19 @@ def trainNetwork(model,args):
         action_index = 0
         r_t = 0
         a_t = np.zeros([ACTIONS])
-        #choose an action epsilon greedy
+        # 经典的epsilon贪心策略
         if t % FRAME_PER_ACTION == 0:
             if random.random() <= epsilon:
                 print("----------Random Action----------")
                 action_index = random.randrange(ACTIONS)
                 a_t[action_index] = 1
             else:
-                q = model.predict(s_t)       #input a stack of 4 images, get the prediction
+                q = model.predict(s_t)  # 输入四幅图，出结果
                 max_Q = np.argmax(q)
                 action_index = max_Q
                 a_t[max_Q] = 1
 
-        #We reduced the epsilon gradually
+        # 逐渐将epsilon缩小，以求得最大Q
         if epsilon > FINAL_EPSILON and t > OBSERVE:
             epsilon -= (INITIAL_EPSILON - FINAL_EPSILON) / EXPLORE
 
@@ -122,18 +125,18 @@ def trainNetwork(model,args):
         x_t1_colored, r_t, terminal = game_state.frame_step(a_t)
 
         x_t1 = skimage.color.rgb2gray(x_t1_colored)
-        x_t1 = skimage.transform.resize(x_t1,(80,80))
-        x_t1 = skimage.exposure.rescale_intensity(x_t1, out_range=(0, 255))
+        x_t1 = skimage.transform.resize(x_t1,(80,80)) #变换维度大小
+        x_t1 = skimage.exposure.rescale_intensity(x_t1, out_range=(0, 255))#
 
         x_t1 = x_t1.reshape(1, x_t1.shape[0], x_t1.shape[1], 1) #1x80x80x1
         s_t1 = np.append(x_t1, s_t[:, :, :, :3], axis=3)
 
-        # store the transition in D
+        # 将状态转移储存到D队列中
         D.append((s_t, action_index, r_t, s_t1, terminal))
         if len(D) > REPLAY_MEMORY:
             D.popleft()
 
-        #only train if done observing
+        # 观测足够之后，开始训练
         if t > OBSERVE:
             #sample a minibatch to train on
             minibatch = random.sample(D, BATCH)
@@ -169,14 +172,14 @@ def trainNetwork(model,args):
         s_t = s_t1
         t = t + 1
 
-        # save progress every 10000 iterations
+        # 每1000次保存训练结果
         if t % 1000 == 0:
             print("Now we save model")
             model.save_weights("model.h5", overwrite=True)
             with open("model.json", "w") as outfile:
                 json.dump(model.to_json(), outfile)
 
-        # print info
+        # 输出信息
         state = ""
         if t <= OBSERVE:
             state = "observe"
@@ -185,8 +188,8 @@ def trainNetwork(model,args):
         else:
             state = "train"
 
-        print("TIMESTEP", t, "/ STATE", state, \
-            "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t, \
+        print("时间步长", t, "/ 状态", state, \
+            "/ EPSILON", epsilon, "/ 动作", action_index, "/ 奖励", r_t, \
             "/ Q_MAX " , np.max(Q_sa), "/ Loss ", loss)
 
     print("Episode finished!")
@@ -197,7 +200,7 @@ def playGame(args):
     trainNetwork(model,args)
 
 def main():
-    parser = argparse.ArgumentParser(description='Description of your program')
+    parser = argparse.ArgumentParser(description='小游戏')
     parser.add_argument('-m','--mode', help='Train / Run', required=True)
     args = vars(parser.parse_args())
     playGame(args)
